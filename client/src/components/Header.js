57918c28deb2d29e9c788e3a82df99ba2f70a104
David Francis <davidjayfrancis@gmail.com>
2019-11-09 10:38:08 -0800

    setting up some components

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PlayerList from "./PlayerList";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>World Soccer Stats</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayerList = props => {
+  return (
+    <div>
+      <p>This is the PLayerList. Add code! </p>
+    </div>
+  );
+};
+
+export default PlayerList;
