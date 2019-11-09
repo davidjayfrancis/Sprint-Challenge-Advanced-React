bc8a6e08a603d17396f49907b59326f1a0043d2b
David Francis <davidjayfrancis@gmail.com>
2019-11-09 11:02:57 -0800

    added axios call

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,35 @@ import React from "react";
 import logo from "./logo.svg";
 import Header from "./components/Header.js";
 import PlayerList from "./components/PlayerList.js";
+import axios from "axios";
+
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  // Let's put the axios call here for now, and later we can
+  // re-factor into a hook
+
+  componentDidMount() {
+    const getPlayers = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/players");
+        this.setState({
+          players: response.data
+        });
+        console.log(this.state.players[5]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPlayers();
+  }
 
   render() {
     return (
